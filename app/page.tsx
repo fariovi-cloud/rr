8c0184c4b827504c24253964dbeb08f112d7b789
fariovi-cloud <fariovi@gmail.com>
2025-08-08 17:42:32 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Page() {
-  const lastUpdate = new Date().toLocaleString();
-  return (
-    <div>
-      <h1>RARI Dashboard v7c</h1>
-      <p>Last update: {lastUpdate}</p>
-    </div>
-  );
-}
